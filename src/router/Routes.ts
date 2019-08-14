4861fd1e67fd796dd3955562257b28ab05adbbe2
Sam Noh <samnoh21@outlook.com>
2019-08-14 15:51:48 +1200

    mod - clean up

--- src/router/Routes.ts
+++ src/router/Routes.ts
@@ -2,18 +2,18 @@ import loadable from '@loadable/component';
 
 export const HomePage = loadable(() =>
     import(/* webpackChunkName: "HomePage" */ '../pages/HomePage').then(
-        module => module.HomePage as any
+        ({ HomePage }) => HomePage as any
     )
 );
 
 export const TodosPage = loadable(() =>
     import(/* webpackChunkName: "TodosPage" */ '../pages/TodosPage').then(
-        module => module.TodosPage as any
+        ({ TodosPage }) => TodosPage as any
     )
 );
 
 export const NotFoundPage = loadable(() =>
     import(/* webpackChunkName: "NotFoundPage" */ '../pages/NotFoundPage').then(
-        module => module.NotFoundPage as any
+        ({ NotFoundPage }) => NotFoundPage as any
     )
 );
